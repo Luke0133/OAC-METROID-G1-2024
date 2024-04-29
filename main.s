0263c08ddbfe6fb6077ef0a7ed272f519e780d9d
Luke0133 <68027676+Luke0133@users.noreply.github.com>
2024-04-29 15:03:40 -0300

    Update main.s

--- main.s
+++ main.s
@@ -10,21 +10,41 @@ PLYR_POS: .half 40, 0, 0, 0 # Guarda a posicao do jogador (topo esquerdo X e Y) 
 .text
 
 SETUP:		
-	la a0, map 		# Endereco do mapa
+#	a0 = Image Address						   #
+#	a1 = X (topo esquerdo) de onde comecar a renderizar a sprite	   #	
+#	a2 = Y (topo esquerdo) de onde comecar a renderizar a sprite	   #	 		  
+#	a3 = largura a ser renderizada					   #
+# 	a4 = altura a ser renderizada					   #
+#	a5 = frame (0 ou 1)						   #
+# --->	a6 = operation (0 if normal printing, 1 if replacing trail)	   #
+#	s8? -> X inicial (topo esquerdo) da renderizacao da sprite	   #
+#       s9? -> Y inicial (topo esquerdo) da renderizacao da sprite 	   #	
+#	s10? -> largura da sprite do mapa				   #
+#	s11? -> altura da sprite do mapa		
+
+	la a0, map1 		# Endereco do mapa
 	li a1, 0		# Topo esquerdo X
 	li a2, 0		# Topo esquerdo Y		
 	li a3, 320		# Largura da imagem
 	li a4, 240		# Altura da imagem	
 	li a5, 0		# Frame = 0
-	call RENDER	
-	la a0, map 		# Endereco do mapa
+	li s8, 0
+	li s9, 0
+	li s10, 960
+	li s11, 240
+	call RENDER_MAP	
+	la a0, map1 		# Endereco do mapa
 	li a1, 0		# Topo esquerdo X
 	li a2, 0		# Topo esquerdo Y		
 	li a3, 320		# Largura da imagem
 	li a4, 240		# Altura da imagem	
 	li a5, 0		# Frame = 0
 	li a5, 1		# Frame = 1
-	call RENDER	
+	li s8, 0
+	li s9, 0
+	li s10, 960
+	li s11, 240
+	call RENDER_MAP	
 	li s0, 0	
 
 ENGINE_SETUP:
@@ -80,43 +100,6 @@ GAME_LOOP:
 
 	j ENGINE_LOOP	# Volta para ENGINE_LOOP
 
-RENDER:
-	#Propper rendering
-
-	li t0,0xFF0	# t0 = 0xFF0
-	add t0,t0,a5	# Printing address corresponds to 0x0FF0 + frame
-	slli t0,t0,20	# shifts 20 bits, making printing adress correct (0xFF00 0000 or 0xFF10 0000)
-	
-	add t0,t0,a1	# t0 = 0xFF00 0000 + X or 0xFF10 0000 + X
-	
-	li t4,320	# t4 = 320
-	mul t4,t4,a2	# t4 = 320 * Y 
-	add t0,t0,t4	# t0 = 0xFF00 0000 + X + (Y * 320) or 0xFF10 0000 + X + (Y * 320)
-	
-	addi a0,a0,8			# t1 = a0 + 8
-	
-	mv t2,zero	# t2 = 0
-	mv t3,zero	# t3 = 0
-	
-	PRINT_LINE:
-		
-			
-		lw t4,0(a0)	# loads word(4 pixels) on t4
-		sw t4,0(t0)	# prints 4 pixels from t4
-		
-		addi t0,t0,4	# increments bitmap address
-		addi a0,a0,4	# increments image address
-		
-		addi t3,t3,4	# increments column counter
-		blt t3,a3,PRINT_LINE	# if column counter < width, repeat
-		
-		addi t0,t0,320
-		sub t0,t0,a3
-		
-		mv t3,zero		# t3 = 0
-		addi t2,t2,1		# increments line counter
-		bgt a4,t2,PRINT_LINE	#if height > line counter, repeat
-		ret	
 
 INPUT_CHECK:
 	li t1,0xFF200000 # Endereco do controle KDMMIO
@@ -194,10 +177,10 @@ INPUT_CHECK:
 		j END_INPUT_CHECK
 	END_INPUT_CHECK:
 		ret				
-					
-.include "SYSTEMv21.s"
 
+.include "render.s"										
+.include "SYSTEMv21.s"
 # Sprites
 .data
-.include "sprites/data/walk_right.data"
-.include "sprites/data/map.data"
+.include "sprites/walk_right.data"
+.include "sprites/map1.data"
