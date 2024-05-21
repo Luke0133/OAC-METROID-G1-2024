22cd12643a68500dc8441d8e1ab2a24c4d07f907
jpantonow <129221613+jpantonow@users.noreply.github.com>
2024-05-21 10:29:30 -0300

    Update teclado.s

--- teclado.s
+++ teclado.s
@@ -26,6 +26,7 @@ INPUT_CHECK:
         CHECK_INPUT.D:
         li t1, 'd'	# Loads ascii value of 'd' key
         bne t0,t1, CHECK_INPUT.SPACE
+        
         j INPUT.D	# If 'd' key was pressed
         
         CHECK_INPUT.SPACE:
@@ -69,31 +70,6 @@ INPUT_CHECK:
 	INPUT.D: # Moves player right
 		la t0, PLYR_POS 
 	        lh t1, 0(t0)
-
-                la a0, Map2 		# Map Address
-                li a1, 0		# starting X on Matrix (top left)
-                li a2, 29		# starting Y on Matrix (top left)		
-                li a3, 0		# X offset (0, 4, 8, 12)
-                li a4, 8		# Y offset (0, 4, 8, 12)	
-                li a5, 0		# Frame = 0
-                li a6, m_screen_width	# Screen Width = 20
-                li a7, m_screen_height	# Screen Height = 15
-                mv t3, t1		# Starting X for rendering (top left, related to Matrix)
-                lb t2, 4(t0)
-                call RENDER_MAP
-
-                la a0, Map2 		# Map Address
-                li a1, 0		# starting X on Matrix (top left)
-                li a2, 29		# starting Y on Matrix (top left)		
-                li a3, 0		# X offset (0, 4, 8, 12)
-                li a4, 8		# Y offset (0, 4, 8, 12)	
-                li a5, 1		# Frame = 1
-                li a6, m_screen_width	# Screen Width = 20
-                li a7, m_screen_height	# Screen Height = 15
-                mv t3, t1		# Starting X for rendering (top left, related to Matrix)
-                lb t2,4(t0)
-                call RENDER_MAP
-
                 addi t1, t1, 4
 	        sh t1,0(t0)
 
@@ -102,42 +78,9 @@ INPUT_CHECK:
                 addi t1,zero,1
                 sb t1, 0(t0)
 
-                								
-                la a0, sam_walk_vertical 		# Gets sprite address# Endereco do mapa
-                la t0,PLYR_POS
-                lh a1, 0(t0)		# Topo esquerdo X
-                lb a2, 4(t0)		# Topo esquerdo Y		
-                li a3, 20		# Largura da imagem
-                li a4, 32		# Altura da imagem	
-                mv a5, s0		# Frame
-                la t0, PLYR_STATUS
-                lb a6, 0(t0) 
-                li a7, 0
-                
-                call RENDER					
-                                                                                
-                li t0,0xFF200604		# carrega em t0 o endereco de troca de frame
-                sw s0,0(t0)
-                
-                ##### LIMPEZA DE RASTRO
-                
-                mv a5, s0		# Frame
-                mv a5,s0		# carrega o frame atual (que esta na tela em a3)
-                xori a5,a5,1		# inverte a3 (0 vira 1, 1 vira 0)
-                
-                la a0, sam_walk_vertical 	# Gets sprite address
-                la t0,PLYR_POS
-                lh a1, 0(t0)		# Topo esquerdo X
-                lb a2, 4(t0)		# Topo esquerdo Y		
-                li a3, 20		# Largura da imagem
-                li a4, 32		# Altura da imagem	
-                mv a5, s0		# Frame
-                la t0, PLYR_STATUS
-                lb a6, 0(t0) 
-                li a7, 0
-                
-                call RENDER
-                
+                li t1, 0
+                sb t1,2(t0)
+
                 j END_INPUT_CHECK
 	
 	INPUT.SPACE:
