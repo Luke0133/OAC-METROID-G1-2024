1329fafbd398940a631664ef7d3497e87a7e6e5f
jpantonov <jpantonow@gmail.com>
2024-06-13 10:36:01 -0300

    sprite_op prototype

--- helpers/sprite_op.s
+++ helpers/sprite_op.s
@@ -16,133 +16,80 @@ UPDATE_STATUS:
 SPRITE_BALL:
     la t1, 0(t0) # Loads sprite number
     li t2, 3 # t2 = max_sprite
-    beq t1,t2,RESET_SPRITE # t1 == t2 ? RESET_SPRITE : INC_SPRITE
+    beq t1,t2,RESET_SPRITE_BALL # t1 == t2 ? RESET_SPRITE : INC_SPRITE
     addi t1,t1,1 # INC_SPRITE
     sh t1, 0(t0) # Store sprite number in status
+    j CHECK_MOVE_X
+
+RESET_SPRITE_BALL:
+    sh zero, 0(t0) # Reset sprite number
 
 CHECK_MOVE_Y:
-    la t1, 2(t0) #
-    bnez t1, SPRITE_UP_FREEFALL
-    j CHECK_MOVE_X
+    la t1, 2(t0) # Check if samus is facing up
+    bnez t1, SPRITE_UP_GROUND # t1 != 0 ? SPRITE_UP_GROUND
+    la t1, 3(t0) # Check if samus is falling
+    bnez t1, SPRITE_FALLING # t1 != 0 ? SPRITE_FALLING
+    j CHECK_MOVE_X # Check for movement in horizontal direction
+
+SPRITE_UP_GROUND:
+    la t1, 3(t0) # Check if samus is falling 
+    bnez t1, SPRITE_UP_FREEFALL # t1 != 0 ? SPRITE_UP_FREEFALL
+
+    la t1, 0(t0) # Loads sprite number
+    # Max-sprite-up = 1
+    bnez t1, RESET_SPRITE_UP # t1 == 1 ? RESET_SPRITE_UP : INC SPRITE_UP_GROUND
+    addi t1,t1,1 # INC SPRITE_UP_GROUND
+    sh t1, 0(t0) # Store sprite number in status
+    j CHECK_MOVE_X # Check for movement in horizontal direction
+
 
 SPRITE_UP_FREEFALL:
     la t1, 0(t0) # Loads sprite number
-    bnez t1, SPRITE_BALL 
-    addi t1, t1, 1 # INC_SPRITE
-    sh t1, 0(t0) # Store sprite number in status 
+    # Max-sprite-up = 1
+    bnez t1, RESET_SPRITE_UP # t1 != 0 ? RESET_SPRITE_UP : INC_SPRITE_UP_FREEFALL
+    addi t1, t1, 1 # INC_SPRITE_UP_FREEFALL
+    sh t1, 0(t0) # Store sprite number in status
+    j CHECK_MOVE_X 
+
+RESET_SPRITE_UP:
+    sh zero, 0(t0) # Reset sprite number
+    j CHECK_MOVE_X # movex or end update status?
 
 CHECK_MOVE_X:
-    la t1,MOVE_X
-    lb t1, 0(t1)
-    blt zero, t1, SPRITE_LEFT 
+    la t1,MOVE_X # Loads Move X
+    lb t1, 0(t1) # Loads Move X byte
+    blt zero, t1, SPRITE_LEFT # zero < moveX ? SPRITE_LEFT : SPRITE_RIGHT 
     j SPRITE_RIGHT
 
 SPRITE_LEFT:
-    la t1, 0(t0)   #loads sprite number
-    li t2,2        # t2 = end of sprites
-    blt t1,t2, ASC_LEFT_SPRITE # t1 < t2 ? ASC_ESQ_SPRITE  
-    j DESC_LEFT_SPRITE
+    la t1, 0(t0)   # Loads sprite number
+    li t2,2    # t2 = max sprite-number
+    blt t1,t2, ASC_LEFT_SPRITE # t1 < t2 ? ASC_ESQ_SPRITE  : DESC_LEFT_SPRITE
+    j RESET_SPRITE_LEFT
 
 ASC_LEFT_SPRITE:
-    addi t1,t1,1 # increments sprite number
-    sb t1, 0(t0) # stores in PLYR_POS
-    j SPRITE_UP_GROUND
+    addi t1,t1,1 # Increments sprite number
+    sb t1, 0(t0) # Stores sprite_number in PLYR_STATUS
+    j END_UPDATE_STATUS
         
-DESC_LEFT_SPRITE:
+RESET_SPRITE_LEFT:
     sb zero, 0(t0) # returns to sprite 0
-    j SPRITE_UP_GROUND
-
-
+    j END_UPDATE_STATUS
+    
 SPRITE_RIGHT:
-    la t1, 0(t0)   #loads sprite number
+    la t1, 0(t0)   # Loads sprite number
     li t2,2        # t2 = end of sprites
     blt t1,t2, ASC_RIGHT_SPRITE # t1 < t2 ? ASC_RIGHT_SPRITE  
-    j DESC_RIGHT_SPRITE
+    j RESET_SPRITE_RIGHT
 
 ASC_RIGHT_SPRITE:
     addi t1,t1,1 # increments sprite number
     sb t1, 0(t0) # stores in PLYR_POS
-    j SPRITE_UP_GROUND
+    j END_UPDATE_STATUS
         
-DESC_RIGHT_SPRITE:
+RESET_SPRITE_RIGHT:
     sb zero, 0(t0) # returns to sprite 0
-    j SPRITE_UP_GROUND
-    
-
+  
 END_UPDATE_STATUS:
     ret
     
-############### old sprite_op #######################
-
-    la t0, PLYR_STATUS # Loads PLYR_STATUS
-    la t1, MOVE_X      # Loads MOVE_X address
-    lb t1, 0(t1)       # Loads MOVE_X byte on t1
-    beqz t1, CHECK_SPRITE_UP # If there's no X movement
-    blt zero, t1,SPRITE_LEFT # a0 < t1 ? SPRITE_LEFT : SPRITE_RIGHT
-    j SPRITE_RIGHT
-
-    CHECK_SPRITE_UP:
-        lb t1, 3(t0)
-        bnez t1, JUMP_SPRITE_UP 
-        j CHECK_SPRITE_VERTICAL
-
-    JUMP_SPRITE_UP:
-        j SPRITE_UP_GROUND
-
-    SPRITE_LEFT:
-        la t1, 0(t0)   #loads sprite number
-        li t2,2        # t2 = end of sprites
-        blt t1,t2, ASC_LEFT_SPRITE # t1 < t2 ? ASC_ESQ_SPRITE  
-        j DESC_LEFT_SPRITE
-
-        ASC_LEFT_SPRITE:
-            addi t1,t1,1 # increments sprite number
-            sb t1, 0(t0) # stores in PLYR_POS
-            j SPRITE_UP_GROUND
-        
-        DESC_LEFT_SPRITE:
-            sb zero, 0(t0) # returns to sprite 0
-            j SPRITE_UP_GROUND
-
-
-    SPRITE_RIGHT:
-        la t1, 0(t0)   #loads sprite number
-        li t2,2        # t2 = end of sprites
-        blt t1,t2, ASC_RIGHT_SPRITE # t1 < t2 ? ASC_RIGHT_SPRITE  
-        j DESC_RIGHT_SPRITE
-
-        ASC_RIGHT_SPRITE:
-            addi t1,t1,1 # increments sprite number
-            sb t1, 0(t0) # stores in PLYR_POS
-            j SPRITE_UP_GROUND
-        
-        DESC_RIGHT_SPRITE:
-            sb zero, 0(t0) # returns to sprite 0
-            j SPRITE_UP_GROUND
-    
-    SPRITE_UP_GROUND:
-        la t1, 0(t0)
-        li t2,1
-        blt t1,t2, ASC_UP_SPRITE
-        j DESC_UP_SPRITE
-
-        ASC_UP_SPRITE:
-
-
-        DESC_UP_SPRITE:
-
-
-    SPRITE_UP_FREEFALL:
-        la t1, 0(t0)
-        li t2,1
-        blt t1,t2, ASC_UP_SPRITE
-        j DESC_UP_SPRITE
-
-        ASC_UP_SPRITE:
-
-
-        DESC_UP_SPRITE:
-
-
-    END_UPDATE_STATUS:
-        ret
